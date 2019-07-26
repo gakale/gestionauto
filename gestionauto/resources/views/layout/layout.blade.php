b01ae079bad035682d41a029c762890874d1542a
gnakale <gnakaleroland@gmail.com>
2019-07-27 01:28:44 +0200

    Debut de modificationn des different liens utilisateur

--- gestionauto/resources/views/layout/layout.blade.php
+++ gestionauto/resources/views/layout/layout.blade.php
@@ -199,11 +199,11 @@
             <i class="fa fa-files-o"></i>
             <span>Gestion Utilisateur</span>
             <span class="pull-right-container">
-              <span class="label label-primary pull-right">4</span>
+              <span class="bg-white pull-right"></span>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
+            <li><a href="pages/layout/top-nav.html"><i class="fa fa-user"></i>Utilisateurs</a></li>
             <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
             <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
             <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>

1384c58d257da4cbc66051930e7e868e4fcc7aa8
gnakale <gnakaleroland@gmail.com>
2019-07-27 01:17:52 +0200

    Debut de modificationn des different liens d sibbar du template par nous

--- gestionauto/resources/views/layout/layout.blade.php
+++ gestionauto/resources/views/layout/layout.blade.php
@@ -45,9 +45,9 @@
     <!-- Logo -->
     <a href="index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>A</b>LT</span>
+      <span class="logo-mini"><b>Pa</b>ck</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Admin</b>LTE</span>
+      <span class="logo-lg"><b>Pack</b>Auto</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
@@ -184,20 +184,20 @@
         <li class="header">MAIN NAVIGATION</li>
         <li class="active treeview">
           <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+            <i class="fa fa-dashboard"></i> <span>Tableau de Bord</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Tableau de Bord</a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i>Tableau de Bord</a></li>
           </ul>
         </li>
         <li class="treeview">
           <a href="#">
             <i class="fa fa-files-o"></i>
-            <span>Layout Options</span>
+            <span>Gestion Utilisateur</span>
             <span class="pull-right-container">
               <span class="label label-primary pull-right">4</span>
             </span>

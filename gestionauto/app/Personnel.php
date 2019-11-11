5b8a8a490db6587b21087d14661bb72c2a939c4d
gnakale <gnakaleroland@gmail.com>
2019-11-11 14:51:40 +0100

    erreur de massagniment

--- gestionauto/app/Personnel.php
+++ gestionauto/app/Personnel.php
@@ -6,9 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Personnel extends Model
 {
+
+
+    protected $fillable = ['name','prenom','email','fonction','telephone'];
+
+
     public function missions()
     {
         return $this->hasMany(Mission::class);
     }
-    }
+
+
+}
 
